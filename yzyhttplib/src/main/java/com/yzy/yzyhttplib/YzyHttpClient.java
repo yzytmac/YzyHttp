c893d3d89f4b6396e5cc26c52551a9c9a40babf8
yzytmac <yzytmac@163.com>
2017-12-18 17:18:44 +0800

    添加javadoc

--- yzyhttplib/src/main/java/com/yzy/yzyhttplib/YzyHttpClient.java
+++ yzyhttplib/src/main/java/com/yzy/yzyhttplib/YzyHttpClient.java
@@ -9,6 +9,14 @@ import java.util.concurrent.FutureTask;
  */
 
 public class YzyHttpClient {
+    /**
+     * 发起网络请求
+     * @param urlString 请求地址的url字符串
+     * @param args 请求参数
+     * @param pType 返回值数据实体类的Type
+     * @param listener 网络请求回调
+     * @param <M> 泛型
+     */
     public static <M> void request(String urlString, HashMap<String,String> args, Type pType, IHttpListener<M> listener){
         HttpTask vHttpTask = new HttpTask(urlString, args, pType,listener);
         ThreadPoolManager.getInstance().excute(new FutureTask<Object>(vHttpTask,null));
